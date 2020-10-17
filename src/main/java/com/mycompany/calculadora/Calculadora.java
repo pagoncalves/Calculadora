9e7412682a6cdcb8de492645c7b4b364dcc6e4c9
Patricia Gonçalves <patricigpereira89@gmail.com>
2020-10-17 16:25:09 -0300

    Versão inicial

---
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.calculadora;
+
+public class Calculadora {
+    
+    static double soma(double a, double b){
+       return a + b;
+    } 
+    
+    static double mult(double a, double b){
+        return a * b;
+    }
+    
+    
+    
+}
---
+++ src/test/java/com/mycompany/calculadora/CalculadoraTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.calculadora;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculadoraTest {
+    
+    public CalculadoraTest() {
+    }
+
+      @Test
+    public void testSoma() {
+      double soma = Calculadora.soma(25.5, 35.6);
+          assertEquals(61.0, soma,1.1);
+             
+    
+}
+    
+    public void testMult(){
+        double mult = Calculadora.mult(55.5, 47.0);
+        assertEquals(55.5*47.0, mult,1.1) ; 
+    }
+}
